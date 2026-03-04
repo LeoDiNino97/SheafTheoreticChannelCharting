5b4982e6cc2135b50f640af3225a1ee010d96056
JRhin <github@jrhin.com>
2026-03-04 15:20:41 +0100

    added val and test dataloader logic

--- src/datamodule.py
+++ src/datamodule.py
@@ -35,7 +35,11 @@ class CSIDataModule(L.LightningDataModule):
         'window': 3,
         'include_same_user_outside_window': False,
         'p_positive': 0.5,  # for contrastive
-        'seed': 0,
+        'train_seed': 27,
+        'test_seed': 42,
+        'val_seed': 123,
+        'train_split': 0.8,
+        'val_split': 0.2,
     }
 
     def __init__(self, dataset_cfg: DictConfig | dict[str, Any]):
@@ -43,6 +47,14 @@ class CSIDataModule(L.LightningDataModule):
         self.cfg = _merge_defaults(self.DEFAULTS, dataset_cfg)
         self.train_dataset = None
         self.test_dataset = None
+        self.val_dataset = None
+
+        assert 0 <= self.cfg['train_split'] <= 1, (
+            'The "train_split" must be between 0 and 1.'
+        )
+        assert 0 <= self.cfg['val_split'] <= 1, (
+            'The "val_split" must be between 0 and 1.'
+        )
 
     def prepare_data(self) -> None:
         if self.cfg['download'] and hasattr(dm, 'download'):
@@ -67,13 +79,23 @@ class CSIDataModule(L.LightningDataModule):
         per_sample_complex = int(np.prod(ch.shape[1:]))
         self.feature_dim = 2 * per_sample_complex
 
+        train_num_users = int(
+            int(self.cfg['num_users']) * (self.cfg['train_split'])
+        )
+        test_num_users = int(
+            int(self.cfg['num_users']) * (1 - self.cfg['train_split'])
+        )
+        val_num_users = int(
+            int(self.cfg['num_users']) * (self.cfg['val_split'])
+        )
+
         self.train_dataset = TrajectoryCSIDataset(
             rx_pos=ds0.rx_pos,
             H_users=ds0.channels,
-            num_users=int(self.cfg['num_users']),
+            num_users=train_num_users,
             T_min=int(self.cfg['T_min']),
             T_max=int(self.cfg['T_max']),
-            seed=int(self.cfg['seed']),
+            seed=int(self.cfg['train_seed']),
             pair_mode=str(self.cfg['pair_mode']),
             window=int(self.cfg['window']),
             include_same_user_outside_window=bool(
@@ -85,10 +107,10 @@ class CSIDataModule(L.LightningDataModule):
         self.test_dataset = TrajectoryCSIDataset(
             rx_pos=ds0.rx_pos,
             H_users=ds0.channels,
-            num_users=int(self.cfg['num_users']),
+            num_users=test_num_users,
             T_min=int(self.cfg['T_min']),
             T_max=int(self.cfg['T_max']),
-            seed=int(self.cfg['seed']),
+            seed=int(self.cfg['test_seed']),
             pair_mode=str(self.cfg['pair_mode']),
             window=int(self.cfg['window']),
             include_same_user_outside_window=bool(
@@ -97,11 +119,64 @@ class CSIDataModule(L.LightningDataModule):
             p_positive=float(self.cfg['p_positive']),
         )
 
+        self.val_dataset = TrajectoryCSIDataset(
+            rx_pos=ds0.rx_pos,
+            H_users=ds0.channels,
+            num_users=val_num_users,
+            T_min=int(self.cfg['T_min']),
+            T_max=int(self.cfg['T_max']),
+            seed=int(self.cfg['val_seed']),
+            pair_mode=str(self.cfg['pair_mode']),
+            window=int(self.cfg['window']),
+            include_same_user_outside_window=bool(
+                self.cfg['include_same_user_outside_window']
+            ),
+            p_positive=float(self.cfg['p_positive']),
+        )
+
+        return None
+
     def train_dataloader(self) -> DataLoader:
+        """The function returns the train DataLoader.
+
+        Returns:
+            DataLoader
+                The train DataLoader.
+        """
         return DataLoader(
             self.train_dataset,
             batch_size=int(self.cfg['batch_size']),
             shuffle=bool(self.cfg['shuffle']),
+            num_workers=int(self.cfg['num_workers']),
+            pin_memory=bool(self.cfg['pin_memory']),
+        )
+
+    def test_dataloader(self) -> DataLoader:
+        """The function returns the test DataLoader.
+
+        Returns:
+            DataLoader
+                The test DataLoader.
+        """
+        return DataLoader(
+            self.test_dataset,
+            batch_size=int(self.cfg['batch_size']),
+            shuffle=False,
+            num_workers=int(self.cfg['num_workers']),
+            pin_memory=bool(self.cfg['pin_memory']),
+        )
+
+    def val_dataloader(self) -> DataLoader:
+        """The function returns the validation DataLoader.
+
+        Returns:
+            DataLoader
+                The validation DataLoader.
+        """
+        return DataLoader(
+            self.val_dataset,
+            batch_size=int(self.cfg['batch_size']),
+            shuffle=False,
             num_workers=int(self.cfg['num_workers']),
             pin_memory=bool(self.cfg['pin_memory']),
         )
